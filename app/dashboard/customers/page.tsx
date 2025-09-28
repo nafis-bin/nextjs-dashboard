12dc996de68b4f5d32cbb5ef301a5d456143fb3f
nafis bin <nafisbinnizam@gmail.com>
2025-09-28 10:34:46 +0600

    feat: navigation solved

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <div className="mx-auto mt-[100px] font-medium text-gray-700 w-[900px] h-[300px] bg-gray-200 overflow-hidden text-3xl flex items-center justify-center hover:bg-gray-400 hover:text-gray-800">customers page</div>
+    );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="w-[900px] h-[300px] mx-auto mt-[200px] bg-gray-200 text-gray-800 
+            text-3xl font-medium flex items-center justify-center hover:bg-gray-400 hover:text-gray-900">
+            invoices page
+        </div>
+    );
+}
